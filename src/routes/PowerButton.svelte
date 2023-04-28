6cb5ca18b07b94df06d7f2749bf3879d9c6a17af
benjorama <benmacomb@gmail.com>
2023-04-28 08:16:49 -0600

    Adds button that plays a pitch on click

---
+++ src/routes/PowerButton.svelte
@@ -0,0 +1,13 @@
+<script>
+	import * as Tone from 'tone';
+
+	function handleClick() {
+		//create a synth and connect it to the main output (your speakers)
+		const synth = new Tone.Synth().toDestination();
+
+		//play a middle 'C' for the duration of an 8th note
+		synth.triggerAttackRelease('C4', '8n');
+	}
+</script>
+
+<button on:click={handleClick}> click me! </button>
